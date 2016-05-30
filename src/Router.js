3821c0a556675c4b8ef44d3d41e5d4d377cefb75
jiasm <jiashunming@outlook.com>
2016-05-30 16:10:44 +0800

    添加对参数的支持

--- src/Router.js
+++ src/Router.js
@@ -14,8 +14,9 @@ function Router (win) {
    * @return {String or Router}      如果是get 返回路径的值 如果是set 返回Router实例
    */
   var pwd = self.pwd = function (hash) {
+
     if (hash !== undefined && hash !== null && hash !== false) {
-      location.hash = hash;
+      win.location.hash = hash;
 
       return self;
     }
@@ -36,9 +37,26 @@ function Router (win) {
       throw new Error('callback must be a function');
     }
 
-    var calls = routers[hash] = routers[hash] || [];
+    var call = routers[hash] = routers[hash] || {
+      events: []
+    };
 
-    calls.push(callback);
+    // 如果设置的值存在参数的获取 需要特殊处理
+    if (!call.flag && hash.indexOf(':') >= 0) {
+
+      var params = [];
+
+      call.flag = '$';
+      call.reg = prepend(hash.replace(/\:([^\/]+)?/g, function (_, $1) {
+
+        params.push($1);
+
+        return '([^\/]+)?';
+      }));
+      call.params = params;
+    }
+
+    call.events.push(callback);
 
     return self;
   }
@@ -53,21 +71,106 @@ function Router (win) {
     return self.pwd(hash);
   }
 
+  /**
+   * 重新生成正则匹配的值
+   * @param  {RegExp} old 原始的正则表达式
+   * @return {RegExp}     添加^标记的正则表达式
+   */
+  function prepend (old) {
+
+    if (typeof old === 'string') return prepend(new RegExp(old));
+
+    var source = old.source;
+    // 如果有^标记和$ 直接返回
+    if (source.indexOf('^') === 0 && source.lastIndexOf('$') === (source.length - 1)) return old;
+    return new RegExp('^' + old.source + '$', old.flags);
+  }
+
+  /**
+   * 根据key来从obj中获取匹配的项
+   * @param  {String} key 作为标记的key
+   * @param  {Object} obj 被匹配的项
+   * @return {Array}      被匹配的集合
+   */
+  function extract (key, obj) {
+
+    var result = [];
+
+    for (var hash in obj) {
+
+      var events = obj[hash];
+      // 说明存在于 使用匹配的情况 而且匹配成功了
+      if (events.flag === '$' && events.reg.test(key)) {
+
+        var keys = events.params;
+        var values = key.match(events.reg).slice(1);
+        var params = concat(keys, values);
+
+        if (params) {
+
+          result.push({
+            events: events.events,
+            params: params
+          })
+        }
+      } else if (key === hash) {  // 这个表示为完全的匹配
+
+        result.push({
+          events: events.events
+        });
+      }
+    }
+
+    return result;
+  }
+
+  function concat (keys, values) {
+
+    var len = keys.length;
+    var params = {};
+    var index = 0;
+
+    // key和value的长度相等
+    if (values.length === len) {
+
+      for (; index < len; index++) {
+
+        params[keys[index]] = values[index];
+      }
+
+      return params;
+    }
+
+    return false;
+  }
+
+  /**
+   * 循环触发回调
+   * @param  {Object} events 一个包涵一些回调函数的集合
+   * @param  {Object} param  任意类型 作为参数放在回调中
+   */
+  function fireEvent (events, param) {
+
+    for (var index in events) {
+
+      var call = events[index];
+      call.call(self, param);
+    }
+  }
+
   // hashchange的事件
   function hashHandler () {
 
     var hash = self.pwd();
-    var calls = routers[hash];
+    var events = extract(hash, routers); // routers[hash];
 
-    if (calls) {
-      for (var index in calls) {
+    if (events.length) {
 
-        var call = calls[index];
-        call.call(self);
+      for (var index in events) {
 
+        fireEvent(events[index].events, events[index].params);
       }
     }
-
   }
 
   // 初始化
